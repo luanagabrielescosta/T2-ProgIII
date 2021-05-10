88dc3f861be33327add26b9f61f7e86d851dabd9
Luana Gabriele de Sousa Costa <luanagabrielescosta@gmail.com>
2021-05-10 10:34:14 -0300

    falta o sort

--- src/TrabProg3/votosComparator.cpp
+++ src/TrabProg3/votosComparator.cpp
@@ -11,22 +11,22 @@ using namespace std;
 	// 	}
 		
 	// }
-bool votosComparator::compare(candidato first, candidato second){
-	if(first.getVotosNominaisCandidato() == second.getVotosNominaisCandidato()){
-		aaaaa;
-	}
+// bool votosComparator::compare(candidato first, candidato second){
+// 	if(first.getVotosNominaisCandidato() == second.getVotosNominaisCandidato()){
+// 		aaaaa;
+// 	}
 
-	return first.getVotosNominaisCandidato() - second.getVotosNominaisCandidato();
-}
+// 	return first.getVotosNominaisCandidato() - second.getVotosNominaisCandidato();
+// }
 
-bool votosComparator::compare2(candidato o1, candidato o2) {
-		if(o1.getVotosNominaisCandidato() - o2.getVotosNominaisCandidato() == 0) {
-			return o2.getNumeroPartidoCandidato() - o1.getNumeroPartidoCandidato();
-		}else {
-			return o1.getVotosNominaisCandidato() - o2.getVotosNominaisCandidato();	
-		}
-}
+// bool votosComparator::compare2(candidato o1, candidato o2) {
+// 		if(o1.getVotosNominaisCandidato() - o2.getVotosNominaisCandidato() == 0) {
+// 			return o2.getNumeroPartidoCandidato() - o1.getNumeroPartidoCandidato();
+// 		}else {
+// 			return o1.getVotosNominaisCandidato() - o2.getVotosNominaisCandidato();	
+// 		}
+// }
 
-bool votosComparator::comparePartido(partido o1, partido o2) {
-		return o1.getVotosTotaisPartido() - o2.getVotosTotaisPartido();
-}
+// bool votosComparator::comparePartido(partido o1, partido o2) {
+// 		return o1.getVotosTotaisPartido() - o2.getVotosTotaisPartido();
+// }
