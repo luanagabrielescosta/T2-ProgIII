9ca0622a3ad5278d004f7db8199eb6e604c26bd7
Luana Gabriele de Sousa Costa <luanagabrielescosta@gmail.com>
2021-05-12 17:24:52 -0300

    Eleicao comentado

--- src/TrabProg3/Eleicao.cpp
+++ src/TrabProg3/Eleicao.cpp
@@ -1,3 +1,10 @@
+/*
+// @ Implementação por:
+// @   Eduardo Afonso Ribeiro Rodrigues
+// @   Luana Gabriele de Sousa Costa
+// @ hpp da classe de eleicao
+*/
+
 #include <iostream>
 #include <string>
 #include <list>
@@ -13,66 +20,92 @@ Eleicao::~Eleicao(){
 	// TODO Auto-generated destructor stub
 }
 
+// Função simples explicada no .hpp
 int Eleicao::getNumeroVagaseleicao(){
 	return numeroVagaseleicao;
 }
 
+// Função simples explicada no .hpp
 void Eleicao::setNumeroVagaseleicao(int numeroVagaseleicao){
 	this->numeroVagaseleicao = numeroVagaseleicao;
 }
 
+// Função simples explicada no .hpp
 list< Partido, allocator<Partido> > Eleicao::getPartidoEleicao(){
 	return partidoeleicao;
 }
 
+// Função simples explicada no .hpp
 void Eleicao::setPartidoEleicao(list< Partido, allocator<Partido> > partidoeleicao){
 	this->partidoeleicao = partidoeleicao;
 }
 
+// Função simples explicada no .hpp
 list< Candidato, allocator<Candidato> > Eleicao::getEleitosProporcionalmente(){
 	return eleitosProporcionalmente;
 }
+
+// Função simples explicada no .hpp
 void Eleicao::setEleitosProporcionalmente(list< Candidato, allocator<Candidato> > &eleitosProporcionalmente){
 	this->eleitosProporcionalmente = eleitosProporcionalmente;
 }
+
+// Função simples explicada no .hpp
 list< Candidato, allocator<Candidato> > Eleicao::getMaisVotados(){
 	return maisVotados;
 }
+
+// Função simples explicada no .hpp
 void Eleicao::setMaisVotados(list< Candidato, allocator<Candidato> > &maisVotados){
 	this->maisVotados = maisVotados;
 }
+
+// Função simples explicada no .hpp
 list< Candidato, allocator<Candidato> > Eleicao::getEleitosMajoritariamente(){
 	return eleitosMajoritariamente;
 }
+
+// Função simples explicada no .hpp
 void Eleicao::setEleitosMajoritariamente(list< Candidato, allocator<Candidato> > &eleitosMajoritariamente){
 	this->eleitosMajoritariamente = eleitosMajoritariamente;
 }
+
+// Função simples explicada no .hpp
 list< Candidato, allocator<Candidato> > Eleicao::getNaoEleitosMajoritariamente(){
 	return naoEleitosMajoritariamente;
 }
+
+// Função simples explicada no .hpp
 void Eleicao::setNaoEleitosMajoritariamente(list< Candidato, allocator<Candidato> > &naoEleitosMajoritariamente){
 	this->naoEleitosMajoritariamente = naoEleitosMajoritariamente;
 }
+
+// Função simples explicada no .hpp
 Date Eleicao::getDataeleicao(){
 	return dataeleicao;
 }
+
+// Função simples explicada no .hpp
 void Eleicao::setDataeleicao(Date &dataeleicao){
 	this->dataeleicao = dataeleicao;
 }
 
+// Função simples explicada no .hpp
 void Eleicao::addPartido(Partido &p){
 	this->partidoeleicao.insert(this->partidoeleicao.end(), p);	
 }
 
-Partido Eleicao::retornaPartidoPeloNum(int x){
+// Função que retorna um partido através do número
+Partido Eleicao::retornaPartidoPeloNum(int numero){
 	list< Partido, allocator<Partido> > :: iterator it;
-	list<Candidato> c;
-	string a = "inexistente";
-	Partido trash = Partido(0, 0, a, a, c);
 
+	list<Candidato> c; // Lista vazia para inicialização
+	string a = " "; // String para inicialização
+	Partido trash = Partido(0, 0, a, a, c); // Partido auxiliar inicializado
+
+	// Loop que percorre os partidos da eleição
 	for(it = partidoeleicao.begin(); it != partidoeleicao.end(); ++it){
-		Partido aux = *it;
-		if(x == aux.getNumeroPartido()){
+		if(numero == it->getNumeroPartido()){ // Verificação do número
 			return *it;
 		}
 	}
@@ -80,14 +113,17 @@ Partido Eleicao::retornaPartidoPeloNum(int x){
 	return trash;
 }
 
+// Função simples explicada no .hpp
 int Eleicao::getQtdVagas(){
 	return qtdVagas;
 }
 
+// Função simples explicada no .hpp
 void Eleicao::setQtdVagas(int qtdVagas){
 	this->qtdVagas = qtdVagas;
 }
 
+// Função simples explicada no .hpp
 void Eleicao::printEleicao(){
 	list< Partido, allocator<Partido> > :: iterator it;
 
@@ -98,33 +134,39 @@ void Eleicao::printEleicao(){
 
 }
 
+// Função de inserção de Candidatos no partido
 void Eleicao::insereCandidatos(Candidato c){
 	list<Partido> :: iterator it;
 
+	// Loop na lista de partidos 
 	for(it = partidoeleicao.begin(); it != partidoeleicao.end(); ++it){
-		Partido p = *it;
-		if(c.getNumeroPartidoCandidato() == p.getNumeroPartido()){
-			p.addCandidato(c);
+		if(c.getNumeroPartidoCandidato() == it->getNumeroPartido()){ // Verificação do número do partido
+			it->addCandidato(c);
 		}
 	}
 }
 
+// Função que funciona como contador de partidos
 int Eleicao::retornaNumeroPartidos(){
 	list< Partido, allocator<Partido> > :: iterator it;
-	int count = 0;
+	int count = 0; // Contador inicializado
 
+	// Loop na lista de partidos 
 	for(it = partidoeleicao.begin(); it != partidoeleicao.end(); ++it){
-		count++;
+		count++; // Contador incrementado
 	}
 	
 	return count;
 }
 
+// Função de contagem de votos nominais
 int Eleicao::contaVotosValidos(list<Candidato> c){
-	int count = 0;
+	int count = 0; // Contador inicializado
 	list< Partido, allocator<Partido> > :: iterator it;
+
+	// Loop na lista de partidos 
 	for(it = partidoeleicao.begin(); it != partidoeleicao.end(); ++it){
-		count = count + it->countValidos();
+		count = count + it->countValidos(); // Incrementa o contador com a função de contagem
 	}
 
 	return count;
--- src/TrabProg3/Eleicao.hpp
+++ src/TrabProg3/Eleicao.hpp
@@ -1,16 +1,24 @@
+/*
+// @ Implementação por:
+// @   Eduardo Afonso Ribeiro Rodrigues
+// @   Luana Gabriele de Sousa Costa
+// @ hpp da classe de eleicao
+*/
+
 #ifndef eleicao_h_
 #define eleicao_h_
 
 #include <iostream>
 #include <string>
 #include <list>
-
 #include "Date.hpp"
 #include "Candidato.hpp"
 #include "Partido.hpp"
-
 using namespace std;
 
+// A classe de eleicao possui a gama de listas e dados utilizados durante o codigo
+// Apesar de não haver uma lista geral de candidatos, estão todos dentro de seus
+// respectivos partidos
 class Eleicao{
 	// Atributos
 	int numeroVagaseleicao; 
@@ -30,48 +38,93 @@ class Eleicao{
 	~Eleicao();
 
 	// Métodos
+
+	// Função que retorna o número de vagas da eleicao 
+	// Obs: Atributo - numeroVagaseleicao
 	int getNumeroVagaseleicao();
 
+	// Função que atribui valor ao número de vagas
+	// Obs: -
 	void setNumeroVagaseleicao(int numeroVagaseleicao);
 
+	// Função que retorna a lista de Partidos da eleição
+	// Obs: -
 	list<Partido> getPartidoEleicao();
 
+	// Função que atribui valor à lista de partidos
+	// Obs: -
 	void setPartidoEleicao(list<Partido> partidoeleicao);
 
+	// Função que retorna a lista de Candidatos Eleitos Efetivamente
+	// Obs: -
 	list<Candidato> getEleitosProporcionalmente();
 
+	// Função que atribui valor à lista de Candidatos Eleitos Efetivamente
+	// Obs: -
 	void setEleitosProporcionalmente(list<Candidato> &eleitosProporcionalmente);
 
+	// Função que retorna a lista de Candidatos mais Votados
+	// Obs: -
 	list<Candidato> getMaisVotados();
 
+	// Função que atribui valor à lista de Candidatos mais Votados
+	// Obs: -
 	void setMaisVotados(list<Candidato> &maisVotados);
 
+	// Função que retorna a lista de Candidatos Eleitos Majoritariamente
+	// Obs: -
 	list<Candidato> getEleitosMajoritariamente();
 
+	// Função que atribui valor à lista de Candidatos Eleitos Majoritariamente
+	// Obs: -
 	void setEleitosMajoritariamente(list<Candidato> &eleitosMajoritariamente);
 
+	// Função que retorna a lista de Partidos Candidatos não Eleitos Majoritariamente
+	// Obs: -
 	list<Candidato> getNaoEleitosMajoritariamente();
 
+	// Função que atribui valor à lista de Candidatos não Eleitos Majoritariamente
+	// Obs: -
 	void setNaoEleitosMajoritariamente(list<Candidato> &naoEleitosMajoritariamente);
 
+	// Função que retorna a data da eleição 
+	// Obs: -
 	Date getDataeleicao();
 
+	// Função que atribui valor à data da eleição 
+	// Obs: -
 	void setDataeleicao(Date &dataeleicao);
 
+	// Função que adiciona partidos na lista de partidos
+	// Obs: -
 	void addPartido(Partido &p);
-	
+
+	// Função que busca um partido pelo número
+	// Obs: Utilizada na adição de candidatos
 	Partido retornaPartidoPeloNum(int x);
 
+	// Função que retorna a quantidade de Vagas
+	// Obs: -
 	int getQtdVagas();
 
+	// Função que atribui valor à quantidade de Vagas
+	// Obs: -
 	void setQtdVagas(int qtdVagas);
 
+	// Função que imprime todos os partidos com seus candidatos e dados
+	// Obs: Função usada apenas em testes
 	void printEleicao();
 
+	// Função que insere candidatos em uma lista genérica
+	// Obs: Usada em eleitosProporcionalmente | maisVotados | eleitosMajoritariamente | naoEleitosMajoritariamente
 	void insereCandidatos(Candidato c);
 
+	// Função que retorna o número geral de partidos
+	// Obs: Função usada em testes e verificações
 	int retornaNumeroPartidos();
 
+	// Função que conta todos os votos NOMINAIS - apesar do nome - da eleição
+	// Obs: -
 	int contaVotosValidos(list<Candidato> c);
 };
 
