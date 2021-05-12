c15d155d8a212fe14c22e679be4345cb52e5b7ba
Luana Gabriele de Sousa Costa <luanagabrielescosta@gmail.com>
2021-05-12 20:29:26 -0300

    Partido comentado

--- src/TrabProg3/Partido.cpp
+++ src/TrabProg3/Partido.cpp
@@ -1,3 +1,10 @@
+/*
+// @ Implementação por:
+// @   Eduardo Afonso Ribeiro Rodrigues
+// @   Luana Gabriele de Sousa Costa
+// @ hpp da classe de partidos
+*/
+
 #include <iostream>
 #include <string>
 #include <list>
@@ -6,6 +13,7 @@
 
 using namespace std;
 
+// Constructor
 Partido::Partido(int numeroPartido, int votosLegendaPartido, string &nomePartido, string &siglaPartido, list<Candidato> candidatoPartido){
 	this->candidatoPartido = candidatoPartido;
 	this->numeroPartido = numeroPartido;
@@ -14,71 +22,82 @@ Partido::Partido(int numeroPartido, int votosLegendaPartido, string &nomePartido
 	this->siglaPartido = siglaPartido;
 }
 
+// Destructor 
 Partido::~Partido(){
 	// TODO Auto-generated destructor stub
 }
 
+// Função simples explicada no .hpp
 int Partido::getVotosNominaisTotaisPartido(){
 	return votosNominaisTotaisPartido;
 }
 
+// Função que retorna os votos nominais do primeiro 
 int Partido::getVotosNominaisPrimeiro(){
-	int maisVotado = 0;
+	int maisVotado = 0; // Contador inicializado
 	list< Candidato, allocator<Candidato> > :: iterator it2;
 
+	// Loop na lista de Candidato
 	for(it2 = this->candidatoPartido.begin(); it2 != this->candidatoPartido.end(); ++it2){
-		if(it2->getVotosNominaisCandidato() > maisVotado){
-			maisVotado = it2->getVotosNominaisCandidato();
+		if(it2->getVotosNominaisCandidato() > maisVotado){ // Verifica se o número de votos é maior que o contador
+			maisVotado = it2->getVotosNominaisCandidato(); // Se for, altera o valor
 		}
 	}
 
 	return maisVotado;
 }
 
+// Função simples explicada no .hpp
 void Partido::setVotosNominaisTotaisPartido(int votosNominaisTotaisPartido){
 	this->votosNominaisTotaisPartido = votosNominaisTotaisPartido;
 }
 
-int votosTotaisPartido;
-//int qtdcandidatoEleitos
-
+// Função simples explicada no .hpp
 int Partido::getVotosTotaisPartido(){
 	return votosTotaisPartido;
 }
 
+// Função simples explicada no .hpp
 void Partido::setVotosTotaisPartido(int votosTotaisPartido){
 	this->votosTotaisPartido = votosTotaisPartido;
 }
 
+// Função simples explicada no .hpp
 list<Candidato> Partido::getCandidatoPartido(){
 	return candidatoPartido;
 }
 
+// Função simples explicada no .hpp
 void Partido::setcandidatoPartido(list<Candidato> lista){
 	this->candidatoPartido = lista;
 }
 
-
+// Função simples explicada no .hpp
 int Partido::getNumeroPartido(){
 	return numeroPartido;
 }
 
+// Função simples explicada no .hpp
 int Partido::getVotosLegendaPartido(){
 	return votosLegendaPartido;
 }
 
+// Função simples explicada no .hpp
 string Partido::getNomePartido(){
 	return nomePartido;
 }
 
+// Função simples explicada no .hpp
 string Partido::getSiglaPartido(){
 	return siglaPartido;
 }
 
+// Função simples explicada no .hpp
 void Partido::addCandidato(Candidato c){
 	this->candidatoPartido.push_back(c);
 }
 
+// Função simples explicada no .hpp
 void Partido::printPartido(){
 	list< Candidato, allocator<Candidato> > :: iterator it;
 	for(it = this->candidatoPartido.begin(); it != this->candidatoPartido.end(); ++it){
@@ -92,48 +111,55 @@ void Partido::printPartido(){
 	cout << endl << endl << endl;
 }
 
+// Função que conta o número de votos nominais da eleicao
 int Partido::countVotosNominais(){
-	int totalVotosNominaisPartido = 0;
+	int totalVotosNominaisPartido = 0; // Contador inicializado
 	list< Candidato, allocator<Candidato> > :: iterator it2;
 
+	// Loop da lista de Candidato 
 	for(it2 = this->candidatoPartido.begin(); it2 != this->candidatoPartido.end(); ++it2){
-		if(it2->getDestinoVoto().compare("Válido") == 0){
+		if(it2->getDestinoVoto().compare("Válido") == 0){ // Verifica se o candidato é válido
 			int help = it2->getVotosNominaisCandidato();
-			totalVotosNominaisPartido = totalVotosNominaisPartido + help;
+			totalVotosNominaisPartido = totalVotosNominaisPartido + help; // soma os votos no total
 		}
 	}
 
 	return totalVotosNominaisPartido;
 }
 
+// Função que conta o número de eleitos
 int Partido::countEleitos(){
-	int eleitos = 0;
+	int eleitos = 0; // Constador inicializado
 	list< Candidato, allocator<Candidato> > :: iterator it2;
 
+	// Loop da lista de Candidato 
 	for(it2 = this->candidatoPartido.begin(); it2 != this->candidatoPartido.end(); ++it2){
-		if(it2->getSituacaoCandidato().compare("Eleito") == 0){
-			eleitos++;
+		if(it2->getSituacaoCandidato().compare("Eleito") == 0){ // Verifica se o candidato é eleito
+			eleitos++; // Incrementa o contador
 		}
 	}
 
 	return eleitos;
 }
 
+// Função que verifica se o candidato é válido
 int Partido::validaSituacao(Candidato c){
-	if(c.getDestinoVoto().compare("Válido") == 0){
+	if(c.getDestinoVoto().compare("Válido") == 0){ // Verifica se o candidato é válido
 		return 0;
 	}
 
 	return 1;
 }
 
+// Função que conta o número de válidos
 int Partido::countValidos(){
-	int count = 0;
+	int count = 0; // Contador inicializado
 	list< Candidato, allocator<Candidato> > :: iterator it2;
 
+	// Loop da lista de Candidato 
 	for(it2 = this->candidatoPartido.begin(); it2 != this->candidatoPartido.end(); ++it2){
-		if(it2->getDestinoVoto().compare("Válido") == 0){
-			count = count + it2->getVotosNominaisCandidato();
+		if(it2->getDestinoVoto().compare("Válido") == 0){ // Verifica se o candidato é válido
+			count = count + it2->getVotosNominaisCandidato(); // Soma os votos nominais no total
 		}
 	}
 
--- src/TrabProg3/Partido.hpp
+++ src/TrabProg3/Partido.hpp
@@ -1,3 +1,10 @@
+/*
+// @ Implementação por:
+// @   Eduardo Afonso Ribeiro Rodrigues
+// @   Luana Gabriele de Sousa Costa
+// @ hpp da classe de partidos
+*/
+
 #ifndef partido_hpp_
 #define partido_hpp_
 
@@ -7,6 +14,8 @@
 
 using namespace std;
 
+// A classe de partidos armazena os dados do partido e seus dados específicos
+// com vários métodos auxiliares para a classe de eleicao
 class Partido{
 	// Atributos
 	list<Candidato> candidatoPartido; 
@@ -23,44 +32,84 @@ class Partido{
 	~Partido();
 
 	// Metodos 
+
+	// Função que retorna os votos nominais do Partido
+	// Obs: -
 	int getVotosNominaisTotaisPartido();
 
+	// Função que retorna os votos nominais do primeiro candidato do Partido
+	// Obs: -
 	int getVotosNominaisPrimeiro();
 
+	// Função que atribui valor aos votos nominais do Partido
+	// Obs: -
 	void setVotosNominaisTotaisPartido(int votosNominaisTotaisPartido);
 
-	int votosTotaisPartido;;
+	// Variável de votos
+	int votosTotaisPartido;
 	
+	// Função que retorna o valor dos votos totais
+	// Obs: Atributo - votosTotaisPartido
 	int getVotosTotaisPartido();
 
+	// Função que atribui o valor dos votos totais
+	// Obs: Atributo - votosTotaisPartido
 	void setVotosTotaisPartido(int votosTotaisPartido);
 
+	// Função que retorna a lista de Candidatos do partido
+	// Obs: -
 	list<Candidato> getCandidatoPartido();
 
+	// Função que atribui uma lista à lista de Candidatos do partido
+	// Obs: -
 	void setcandidatoPartido(list<Candidato> lista);
 
+	// Função que retorna o número do partido
+	// Obs: Atributo - numeroPartido
 	int getNumeroPartido();
 
+	// Função que retorna o número de votos de legenda do partido
+	// Obs: Atributo - votosLegendaPartido
 	int getVotosLegendaPartido();
 
+	// Função que retorna o nome do partido
+	// Obs: Atributo - nomePartido
 	string getNomePartido();
 
+	// Função que retorna a sigla do partido
+	// Obs: Atributo - siglaPartido
 	string getSiglaPartido();
 	
+	// Função que adiciona um candidato
+	// Obs: -
 	void addCandidato(Candidato c);
 
+	// Função que imprime o Partido
+	// Obs: -
 	void printPartido();
 
+	// Função de recontagem dos votos nominais
+	// Obs: -
 	int countVotosNominais();
 
+	// Função que conta o número de eleitos
+	// Obs: -
 	int countEleitos();
 
+	// Função que retorna o primeiro Candidato da lista
+	// Obs: -
 	Candidato getPrimeiroCandidato(Candidato primeiro);
 
+	// Função que retorna o último Candidato da lista
+	// Obs: -
 	Candidato getUltimoCandidato(Candidato primeiro);
 
+	// Verifica se um candidato é válido
+	// Obs:  igual retorna 0 | diferente retorna 1
 	int validaSituacao(Candidato c);
 	
+	// Função que conta o número de Válidos
+	// Obs: -
 	int countValidos();
 };
 
