267a5d1d95fe2e0e9fa5b49bee83819dea8cd887
Luana Gabriele de Sousa Costa <luanagabrielescosta@gmail.com>
2021-05-12 19:07:43 -0300

    Lib comentado

--- src/TrabProg3/Lib.cpp
+++ src/TrabProg3/Lib.cpp
@@ -1,3 +1,10 @@
+/*
+// @ Implementação por:
+// @   Eduardo Afonso Ribeiro Rodrigues
+// @   Luana Gabriele de Sousa Costa
+// @ hpp da classe de lib (library)
+*/
+
 #include "Lib.hpp"
 #include <string>
 #include <iostream>
@@ -5,91 +12,100 @@
 
 using namespace std;
 
+// Constructor
 Lib::Lib(){
 }
 
+// Destructor 
 Lib::~Lib(){
 	// TODO Auto-generated destructor stub
 }
 
+// Função que transforma string para inteiro 
 int Lib::stringToInt(string &old){
-    int count;
+    int count = 0; // Contador inicializado
 
+    // Loop pela string
     for(int i = 0; i < old.size(); i++){
-        if(i == 0){
-            count = old[i] - 48;
-        } else{
-            count = count * 10 + (old[i] - 48);
-        }
+        count = count * 10 + (old[i] - 48);
+        // count soma ele mesmo vezes 10 [ ex: 0 x 10 = 0, na primeira posição não soma]
+        // e soma a string - 48, de acordo com a tabela ascii [ ex: 1 -> 49 - 18 = 1]
     }
     
     return count;
 }
 
+// Função que transforma string para Date 
 Date Lib::stringToDate(string &old){
-    int d, m, y;
-    Date a = Date();
+    int d, m, y; // Variável para dia, mês e ano
+    Date a = Date(); // Date auxilia inicializado
 
-    d = (old[0] - 48) * 10 + (old[1] - 48);
-    m = (old[3] - 48) * 10 + (old[4] - 48);
-    y = (old[6] - 48) * 1000 + (old[7] - 48) * 100 + (old[8] - 48) * 10 + (old[9] - 48);
+    d = (old[0] - 48) * 10 + (old[1] - 48); // Pega os 2 primeiros caracteres e faz o esquema da função stringToInt
+    m = (old[3] - 48) * 10 + (old[4] - 48); // Pula a / (posição 2) e pega os 2 próximos caracteres
+    y = (old[6] - 48) * 1000 + (old[7] - 48) * 100 + (old[8] - 48) * 10 + (old[9] - 48); // Pula a / (posição 2) e pega os 4 últimos caracteres
 
+    // Seta a o valor da data 
     a.enterDate(d, m, y);
 
     return a;
 }
 
+// Função simples explicada no .hpp
 char Lib::stringToChar(string &old){
     return old[0];
 }
 
+// Função simples explicada no .hpp
 char Lib::intToChar(int old){
     return (old +'0');
 }
 
+// Retorna int de uma lista 
 int Lib::returnInt(list< int, allocator<int> > &lista, int one){
-    int count = 0;
+    int count = 0; // Contador inicializado
     list< int, allocator<int> > :: iterator it;
 
+    // Loop na lista de int  
     for(it = lista.begin(); it != lista.end();it++){
-		//printa os numeros pares
-		if(count == one){
-            return *it;
+		if(count == one){ // Verifica se chegou na posição
+            return *it; // Se chegou retorna o int
         }
 
-        count++;
+        count++; // Incrementa o contador
 	}
 
     return 0;
 }
 
+// Retorna Candidato de uma lista 
 Candidato Lib::returnCandidato(list<Candidato> lista, Candidato &c, int one){
-    int count = 0;
+    int count = 0; // Contador inicializado
     list< Candidato, allocator<Candidato> > :: iterator it;
 
+    // Loop na lista de Candidato  
     for(it = lista.begin(); it != lista.end();it++){
-		if(count == one){
-            it->printCandidato();
-            c = *it;
-
-            return c;
+		if(count == one){ // Verifica se chegou na posição
+            return *it; // Se chegou retorna o Candidato
         }
 
-        count++;
+        count++; // Incrementa o contador
 	}
 
     return c;
 }
 
+// Retorna a posição de um candidato na lista 
 int Lib::returnPosicao(list<Candidato> lista, string nome){
-    int c = 0;
+    int c = 0; // Contador inicializado
     list< Candidato, allocator<Candidato> > :: iterator it3;
 
+    // Loop na lista de Candidato  
     for(it3 = lista.begin(); it3 != lista.end(); ++it3){
-        if(it3->getNomeCandidato().compare(nome) == 0){
-            return c;
+        if(it3->getNomeCandidato().compare(nome) == 0){ // Verifica o nome
+            return c; // Se chegou retorna a posição do Candidato
         }
-        c++;
+        
+        c++; // Incrementa o contador
     }
     
     return c;
--- src/TrabProg3/Lib.hpp
+++ src/TrabProg3/Lib.hpp
@@ -1,3 +1,10 @@
+/*
+// @ Implementação por:
+// @   Eduardo Afonso Ribeiro Rodrigues
+// @   Luana Gabriele de Sousa Costa
+// @ hpp da classe de lib (library)
+*/
+
 #ifndef lib_h_
 #define lib_h_
 
@@ -6,6 +13,9 @@
 #include "Date.hpp"
 #include "Candidato.hpp"
 
+// A classe Lib foi feita para organizar funções que não eram
+// específicas de nenhuma outra classe (com exceção de uma que
+// mexe com Candidato e era similar a outra dessa classe)
 class Lib{
     public:
     // Constructor
@@ -14,12 +24,33 @@ class Lib{
     ~Lib();
 
     // Métodos
+
+    // Função que muda o tipo da string para int
+	// Obs: -
     int stringToInt(std::string &old);
+
+    // Função que muda o tipo da string para Date
+	// Obs: -
     Date stringToDate(std:: string &old);
+
+    // Função que muda o tipo da string para char
+	// Obs: -
     char stringToChar(std:: string &old);
+
+    // Função que muda o tipo da int para char
+	// Obs: Função principal desta classe
     char intToChar(int old);
+
+    // Retorna um int de uma lista pelo index
+	// Obs: -
     int returnInt(list< int, allocator<int> > &list, int one);
+
+    // Retorna um Candidato de uma lista pelo index
+	// Obs: -
     Candidato returnCandidato(list<Candidato> lista, Candidato &c, int one);
+
+    // Função que retorna a posição de um Candidato na lista
+	// Obs: -
     int returnPosicao(list<Candidato> lista, string nome);
 };
 
